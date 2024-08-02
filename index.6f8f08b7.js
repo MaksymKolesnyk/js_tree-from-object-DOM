290de9bb840eb9bf54a7d05e70d07f639581dabc
MaksymKolesnyk <maksymka@icloud.com>
2024-08-02 00:00:54 -0400

    make build

---
+++ index.6f8f08b7.js
@@ -0,0 +1,2 @@
+!function e(n,t){for(var r in t){var a=document.createElement("li");if(a.textContent=r,n.append(a),Object.keys(t[r]).length>0){var c=document.createElement("ul");a.append(c),e(c,t[r])}}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.6f8f08b7.js.map
